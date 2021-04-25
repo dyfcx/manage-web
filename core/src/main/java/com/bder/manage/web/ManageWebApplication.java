96d8e811f343bf59d89fd0619be5b23af735f8e6
dyfcx <1291009873@qq.com>
2021-04-25 16:34:59 +0800

    删除旧版权限配置

--- core/src/main/java/com/bder/manage/web/ManageWebApplication.java
+++ core/src/main/java/com/bder/manage/web/ManageWebApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * @author Felix YF Dong
+ */
 @SpringBootApplication
-@ComponentScan(basePackages = "com.bder.manage.*")//  @Controller、@Service 注解；
-@EnableJpaRepositories(basePackages = "com.bder.manage.*")//  @Repository 注解；
-@EntityScan(basePackages = "com.bder.manage.*")//  @Entity 注解；
+@ComponentScan(basePackages = "com.bder.manage.*")
+@EnableJpaRepositories(basePackages = "com.bder.manage.*")
+@EntityScan(basePackages = "com.bder.manage.*")
 public class ManageWebApplication {
 
     public static void main(String[] args) {
